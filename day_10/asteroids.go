1b40b23811d18e7540b59fe209eac733ca99fa92
Zhao Shiqi <zhao.shiqi.art@gmail.com>
2019-12-24 12:35:07 -0500

    Complete day 10 part 2

--- day_10/asteroids.go
+++ day_10/asteroids.go
@@ -5,21 +5,23 @@ import (
 	"io/ioutil"
 	"math"
 	"regexp"
+	"sort"
 	"strings"
 )
 
-type Coordinate struct {
+type coordinate struct {
 	x int
 	y int
 }
 
-type Asteroid struct {
-	coordinate       Coordinate
-	lineOfSightCount int
+type asteroid struct {
+	coordinate          coordinate
+	lineOfSightCount    int
+	orderOfVaporization int
 }
 
 func main() {
-	// testMapStr := "#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n.##..###.#\n..#...##..\n..##....##\n......#...\n.####.###."
+	// testMapStr := ".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##"
 
 	// Read map file
 	inputMapStr, e := ioutil.ReadFile("map.txt")
@@ -27,18 +29,19 @@ func main() {
 		panic(e)
 	}
 	// Parse map into coordinates
-	parsedAsteroids := func(asteroidMap string) []Asteroid {
+	parsedAsteroids := func(asteroidMap string) []asteroid {
 		re := regexp.MustCompile(`#`)
 		totalAsteroidsCount := len(re.FindAllString(asteroidMap, -1))
-		asteroids := make([]Asteroid, totalAsteroidsCount)
+		asteroids := make([]asteroid, totalAsteroidsCount)
 		rows := strings.Split(asteroidMap, "\n")
 		asteroidIndex := 0
 		for rowNumber, row := range rows {
 			allAsteroids := re.FindAllStringIndex(row, -1)
 			for _, bounds := range allAsteroids {
-				asteroids[asteroidIndex] = Asteroid{
-					coordinate:       Coordinate{x: bounds[0], y: rowNumber},
-					lineOfSightCount: 0}
+				asteroids[asteroidIndex] = asteroid{
+					coordinate:          coordinate{x: bounds[0], y: rowNumber},
+					lineOfSightCount:    0,
+					orderOfVaporization: 0}
 				asteroidIndex++
 			}
 		}
@@ -49,6 +52,7 @@ func main() {
 	maxLosCount, bestAsteroid := 0, 0
 
 	// Run through each coordinate, see how many asteroids it can see
+
 	for i, curr := range parsedAsteroids {
 		losMap := make(map[float64]struct{})
 		for j, target := range parsedAsteroids {
@@ -74,7 +78,54 @@ func main() {
 			maxLosCount = len(losMap)
 		}
 	}
-	fmt.Print(parsedAsteroids[bestAsteroid])
+	station := parsedAsteroids[bestAsteroid]
+	fmt.Println(station)
+
+	// Vaporize 'em all
+	countToBeVaporized := len(parsedAsteroids) - 1
+	numberVaporized := 0
+	vaporizeAsteroids := func() {
+		losMap := make(map[float64]int)
+		for i, target := range parsedAsteroids {
+			if target.orderOfVaporization != 0 || i == bestAsteroid {
+				continue
+			}
+			var dx, dy float64 = float64(target.coordinate.x - station.coordinate.x),
+				float64(target.coordinate.y - station.coordinate.y)
+			arclength := math.Sqrt(dx*dx + dy*dy)
+			angle := math.Acos(dx / arclength)
+			if dy < 0 {
+				angle = 2*math.Pi - angle
+			}
+			// transform to make clockwise starting from top
+			angle = angle + math.Pi/2
+			if angle >= 2*math.Pi {
+				angle = angle - 2*math.Pi
+			}
+			angle = round(angle, 4)
+			losMap[angle] = i
+		}
+		keysArr := make([]float64, len(losMap))
+		ind := 0
+		for i := range losMap {
+			keysArr[ind] = i
+			ind++
+		}
+		sort.Float64s(keysArr)
+		for _, v := range keysArr {
+			parsedAsteroids[losMap[v]].orderOfVaporization = numberVaporized + 1
+			numberVaporized++
+		}
+	}
+	for numberVaporized < countToBeVaporized {
+		vaporizeAsteroids()
+	}
+	for _, v := range parsedAsteroids {
+		if v.orderOfVaporization == 200 {
+			fmt.Print(v)
+		}
+	}
+
 }
 
 func round(num float64, precision int) float64 {
