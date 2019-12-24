d0cc8247e3804007f7823250b2c90b4023172667
Zhao Shiqi <zhao.shiqi.art@gmail.com>
2019-12-24 10:12:23 -0500

    Refactor day 9 part 1

--- day_9/intcodeMachine.go
+++ day_9/intcodeMachine.go
@@ -7,12 +7,6 @@ import (
 	"strings"
 )
 
-// type amplifierState struct {
-// 	mem       []int64
-// 	currIndex int64
-// 	done      bool
-// }
-
 func main() {
 	file, err := ioutil.ReadFile("boost.txt")
 	check(err)
@@ -23,26 +17,12 @@ func main() {
 		check(err)
 	}
 
-	// amplifierInput := func(phase int64, input int64) func(int64) (int64, error) {
-	// 	return func(callCount int64) (int64, error) {
-	// 		if callCount == 0 {
-	// 			return phase, nil
-	// 		} else if callCount == 1 {
-	// 			return input, nil
-	// 		}
-	// 		return 0, errors.New("Input called more than 2 times")
-	// 	}
-	// }
-
 	integerInput := func(num int) func(int) (int, error) {
 		return func(callCount int) (int, error) {
 			return num, nil
 		}
 	}
 
-	// Part 1*
-
-	// testInput := []int64{104, 1125899906842624, 99}
 	_, out, _, _, _ := executeIntcode(0, numIntcode, integerInput(1), true)
 	fmt.Print(out)
 }
@@ -64,12 +44,8 @@ func executeIntcode(
 	inputCallOnce bool) (index int64, output []int64, mem []int64, done bool, e error) {
 	inputCallCount := 0
 	var relativeBase int64 = 0
-	// out = []int64{}
 	for code[currIndex] != 99 {
 		numericOpcode := code[currIndex]
-		// getTwoParam := func() (int64, int64) {
-		// 	return code[code[currIndex+1]], code[code[currIndex+2]]
-		// }
 		getByIndex := func(pos int64) int64 {
 			if pos >= int64(len(code)) {
 				return 0
@@ -138,13 +114,11 @@ func executeIntcode(
 			params := getParams(3)
 			p1, p2, p3 := params[0], params[1], params[2]
 			code = write(p3, getByIndex(p1)+getByIndex(p2))
-			// code[code[currIndex+3]] = p1 + p2
 			currIndex += 4
 		case 2:
 			params := getParams(3)
 			p1, p2, p3 := params[0], params[1], params[2]
 			code = write(p3, getByIndex(p1)*getByIndex(p2))
-			// code[code[currIndex+3]] = p1 * p2
 			currIndex += 4
 		case 3:
 			params := getParams(1)
@@ -154,16 +128,13 @@ func executeIntcode(
 				return currIndex, output, code, false, nil
 			}
 			code = write(p1, int64(in))
-			// code[p1] = in
 			inputCallCount++
 			currIndex += 2
 		case 4:
 			params := getParams(1)
 			p1 := params[0]
-			// fmt.Println(p1)
 			currIndex += 2
 			output = append(output, getByIndex(p1))
-			// return currIndex, []int64{p1}, code, false, e
 		case 5:
 			params := getParams(2)
 			p1, p2 := params[0], params[1]
@@ -214,32 +185,4 @@ func executeIntcode(
 	}
 
 	return currIndex, output, code, false, e
-}
-
-func permutations(arr []int) [][]int {
-	var helper func([]int, int)
-	res := [][]int{}
-
-	helper = func(arr []int, n int) {
-		if n == 1 {
-			tmp := make([]int, len(arr))
-			copy(tmp, arr)
-			res = append(res, tmp)
-		} else {
-			for i := 0; i < n; i++ {
-				helper(arr, n-1)
-				if n%2 == 1 {
-					tmp := arr[i]
-					arr[i] = arr[n-1]
-					arr[n-1] = tmp
-				} else {
-					tmp := arr[0]
-					arr[0] = arr[n-1]
-					arr[n-1] = tmp
-				}
-			}
-		}
-	}
-	helper(arr, len(arr))
-	return res
 }
